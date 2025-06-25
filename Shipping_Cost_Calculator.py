b03f1d3e5137f9dde98040c44b71365cc4901e23
Htet <yyhtet43@gmail.com>
2025-06-25 20:24:35 +0630

    Added weight and rate info in display section

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -8,5 +8,7 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 shipping_cost = weight * rate
 
 ## Display the result
+print(f"Weight: {weight}kg\n")
+print(f"Rate: {rate}USD per kg\n")
 print(f"Shipping Cost: {shipping_cost} USD")
 
